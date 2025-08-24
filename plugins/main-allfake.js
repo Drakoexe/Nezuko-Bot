4ebb9b9e534ce97ec15584091c8ea61530d1a8f3
Drakoexe <jeinerjoelchavezjuarez5@gmail.com>
2025-08-24 14:22:56 -0500

    main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -37,7 +37,7 @@ global.listo = '❀ *Aquí tienes ฅ^•ﻌ•^ฅ*'
 global.fotoperfil = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1745522645448.jpeg')
 
 global.canalIdM = ["120363401404146384@newsletter", "120363401404146384@newsletter"]
-global.canalNombreM = ["ᰔᩚ ᥡᥙkіᑲ᥆𝗍-mძ • ᥙ⍴ძᥲ𝗍ᥱs ❀", "ᰔᩚ ᥡᥙkіᑲ᥆𝗍-mძ • ᥲ᥎іs᥆s ❀"]
+global.canalNombreM = ["⏤͟͟͞͞ᰔᩚ 𝙽𝚎𝚣𝚞𝚔𝚘 𝙱𝚘𝚝-𝚄𝚙𝚍𝚊𝚝𝚎𝚜 ⏤͟͟͞͞ᰔᩚ", "⏤͟͟͞͞ᰔᩚ 𝙽𝚎𝚣𝚞𝚔𝚘 𝙱𝚘𝚝-𝚄𝚙𝚍𝚊𝚝𝚎𝚜 ⏤͟͟͞͞ᰔᩚ"]
 global.channelRD = await getRandomChannel()
 
 global.d = new Date(new Date + 3600000)
